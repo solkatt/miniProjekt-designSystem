37170efef0cc923af81a70840f41d01bc8dd2a13
solkatt <viktoralexanderodman@gmail.com>
2020-03-25 22:29:51 +0100

    Installed Material UI + Test Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import MyAwesomeReactComponent from './components/MyAwesomeReactComponent';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import AppBarExampleIcon from "./components/Header/AppBar"
+import GridListExampleSimple from './components/ProductGrid/ProductGrid'
+ 
+const App = () => (
+  <MuiThemeProvider>
+    <AppBarExampleIcon />
+    <MyAwesomeReactComponent />
+    < GridListExampleSimple />
+  </MuiThemeProvider>
+);
+ 
 
-export default App;
+
+export default App
