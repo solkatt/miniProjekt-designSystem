63cfdaa3fff3820cff0152754bd18a95a299e227
Simon Beijer <beijersimon@gmail.com>
2020-04-07 20:15:23 +0200

    Updates

---
+++ precache-manifest.76abcca9b8e62400941492b05fbe1ca2.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "847e483a08536156ba285d8c16eb07a8",
+    "url": "/miniProjekt-designSystem/index.html"
+  },
+  {
+    "revision": "bdaee519da50239e75f9",
+    "url": "/miniProjekt-designSystem/static/css/main.5f27be7e.chunk.css"
+  },
+  {
+    "revision": "038fc26f92bad88439fe",
+    "url": "/miniProjekt-designSystem/static/js/2.f0b0c569.chunk.js"
+  },
+  {
+    "revision": "5b8b49146ea7ff4abb4f22a9d28772fe",
+    "url": "/miniProjekt-designSystem/static/js/2.f0b0c569.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bdaee519da50239e75f9",
+    "url": "/miniProjekt-designSystem/static/js/main.3f84d71c.chunk.js"
+  },
+  {
+    "revision": "1e03c7df9a015584772e",
+    "url": "/miniProjekt-designSystem/static/js/runtime-main.8b76efbc.js"
+  },
+  {
+    "revision": "eb04c302e436a2f1e41756837895c9f3",
+    "url": "/miniProjekt-designSystem/static/media/powerline.eb04c302.jpg"
+  },
+  {
+    "revision": "d88aa3a479d9309e92c291dd4fbe2fb3",
+    "url": "/miniProjekt-designSystem/static/media/proanimal.d88aa3a4.png"
+  },
+  {
+    "revision": "4a4a6a79527b488762a40d4b12fff68a",
+    "url": "/miniProjekt-designSystem/static/media/profamily.4a4a6a79.jpg"
+  },
+  {
+    "revision": "8a767ccbed3d66582c8463552ac6285f",
+    "url": "/miniProjekt-designSystem/static/media/silentperformer.8a767ccb.jpg"
+  },
+  {
+    "revision": "8515a90dc36cd19ee89038d2752f4da7",
+    "url": "/miniProjekt-designSystem/static/media/swish_secondary.8515a90d.svg"
+  }
+]);
